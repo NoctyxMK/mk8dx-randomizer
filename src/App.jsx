a377cad922637e77abbdb906db9418feb3e824e8
Noctyx <libeg2005@gmail.com>
2025-09-04 16:12:01 +0200

    update app adding basename

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ItemRandomizer from "./pages/ItemRandomizer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/combos" element={<ComboRandomizer />} />
